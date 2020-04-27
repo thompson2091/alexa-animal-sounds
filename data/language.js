8b1b204c943cc04f5139b0e86a03eed38b508d20
John Thompson <64417502+thompson2091@users.noreply.github.com>
2020-04-27 09:26:37 -0400

    Create language.js

---
+++ data/language.js
@@ -0,0 +1,8 @@
+module.exports  = {
+	'welcome': 	"Welcome to <emphasis>Kids Animal Sounds!</emphasis> Inside you will travel to different parts of the world <break time='300ms'/> to hear and learn about your favorite animals.",
+	'return': 	"Welcome back to <emphasis>Kids Animal Sounds!</emphasis> ",
+	'next': 	"Our next stop is in <emphasis>{animal.country}</emphasis> to visit the <emphasis>{animal.name}</emphasis>",
+	'continue': "Say <emphasis>Continue</emphasis> to visit the {animal.name}.  ",
+	'stop': 	"Thanks for visiting Kids Animal Sounds!  If you liked this skill, please give us a 5 star rating",
+	'repeat': 	"You can say Continue, <break time='300ms'/> <emphasis>List</emphasis> Animals <break time='300ms'/> or Stop to quit."
+};
