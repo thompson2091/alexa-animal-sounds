7414b2b2bfc06060376d157a545732dbbd3d689d
John Thompson <64417502+thompson2091@users.noreply.github.com>
2020-04-27 09:26:10 -0400

    Create airplanes.js

---
+++ data/airplanes.js
@@ -0,0 +1,6 @@
+module.exports  = [
+	"https://s3.amazonaws.com/aws-alexa/guess-the-sound/transportation/airplane.mp3",
+	"https://s3.amazonaws.com/aws-alexa/guess-the-sound/transportation/airplane-3.mp3",
+	"https://s3.amazonaws.com/aws-alexa/guess-the-sound/transportation/airplane-4.mp3",
+	"https://s3.amazonaws.com/aws-alexa/guess-the-sound/transportation/airplane-5.mp3"
+];
